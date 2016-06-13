c7920ce633b30fa63709e2c6e7485762d22457b9
Gavin Price <gaprice@lbl.gov>
2016-06-13 12:31:25 -0700

    forget doing automatic schema updates

--- src/us/kbase/common/schemamanager/SchemaManager.java
+++ src/us/kbase/common/schemamanager/SchemaManager.java
@@ -1,7 +1,7 @@
 package us.kbase.common.schemamanager;
 
 import us.kbase.common.schemamanager.exceptions.InvalidSchemaRecordException;
-import us.kbase.common.schemamanager.exceptions.NoSchemaUpgradePathException;
+import us.kbase.common.schemamanager.exceptions.IncompatibleSchemaException;
 import us.kbase.common.schemamanager.exceptions.SchemaManagerCommunicationException;
 import us.kbase.common.schemamanager.exceptions.UpdateInProgressException;
 
@@ -12,6 +12,19 @@ import com.mongodb.DBObject;
 import com.mongodb.DuplicateKeyException;
 import com.mongodb.MongoException;
 
+/** Checks schema versions and throws errors if the schema version doesn't
+ * match the code version. 
+ * 
+ * This class deliberately does not handle automatic upgrades, as it is too
+ * difficult to automatically upgrade a database if multiple servers are
+ * running. For example, if a v2 server is running and a v3 server is started,
+ * the v3 server will upgrade the database to v3, which could cause v2 to
+ * error, or worse, corrupt the database. Even locking the database while the
+ * upgrade is in progress can't fix this - the v2 server would have to 
+ * detect the upgrade and stop.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class SchemaManager {
 	
 	//these were deliberately set to be compatible with the workspace 
@@ -32,10 +45,10 @@ public class SchemaManager {
 	
 	//TODO NOW tests
 	//TODO NOW check changes so far and list tests necessary
-	public void checkAndUpgradeSchema(
+	public void checkSchema(
 			final String schemaType,
 			final int currentVer)
-			throws InvalidSchemaRecordException, NoSchemaUpgradePathException,
+			throws InvalidSchemaRecordException, IncompatibleSchemaException,
 			UpdateInProgressException, SchemaManagerCommunicationException {
 		if (schemaType == null || schemaType.isEmpty()) {
 			throw new IllegalArgumentException(
@@ -49,14 +62,11 @@ public class SchemaManager {
 		cfg.put(SCHEMA_VER, currentVer);
 		try {
 			schemaCol.insert(cfg);
-			//there's no schema version. An upgrade might be required.
-			upgrade(schemaType, 0, currentVer);
 		} catch (DuplicateKeyException dk) {
 			//ok, the version doc is already there, this isn't the first
 			//startup
-			final DBCursor cur = schemaCol
-					.find(new BasicDBObject(
-							SCHEMA_KEY, schemaType));
+			final DBCursor cur = schemaCol.find(new BasicDBObject(
+					SCHEMA_KEY, schemaType));
 			if (cur.size() != 1) {
 				throw new InvalidSchemaRecordException(
 						"Multiple schema documents found in the database " +
@@ -65,10 +75,9 @@ public class SchemaManager {
 			}
 			final DBObject storedCfg = cur.next();
 			final int dbver = (Integer) storedCfg.get(SCHEMA_VER);
-			if (dbver > currentVer) {
-				throw new NoSchemaUpgradePathException(String.format(
-						"The schema version of the database, %s, is greater " +
-						"than the schema version of the codebase, %s",
+			if (dbver != currentVer) {
+				throw new IncompatibleSchemaException(String.format(
+						"Incompatible database schema. Server is v%s, DB is v%s",
 						dbver, currentVer));
 			}
 			if ((Boolean)storedCfg.get(UPDATE)) {
@@ -76,22 +85,10 @@ public class SchemaManager {
 						"The database is in the middle of an update from " +
 						"version %s of the schema.", dbver));
 			}
-			if (dbver < currentVer) {
-				upgrade(schemaType, dbver, currentVer);
-			}
 		} catch (MongoException me) {
 			throw new SchemaManagerCommunicationException(
 					"There was a problem communicating with the database", me);
 		}
-	}
-
-	private void upgrade(String schemaType, int dbver, int currentVer) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void register(final SchemaUpgrader upgrader) {
-		//TODO NOW register upgrade
 	}
 
 	private void ensureIndexes(DBCollection schemaCol) {
