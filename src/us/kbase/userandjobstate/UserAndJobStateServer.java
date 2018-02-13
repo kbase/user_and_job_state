150f09c06fa7dc1cd5c0425e25e5bc546300891e
Steve Chan <sychan168@gmail.com>
2018-02-13 15:37:47 -0800

    Bump version and release notes

--- src/us/kbase/userandjobstate/UserAndJobStateServer.java
+++ src/us/kbase/userandjobstate/UserAndJobStateServer.java
@@ -146,7 +146,7 @@ public class UserAndJobStateServer extends JsonServerServlet {
 	private static final String GIT =
 			"https://github.com/kbase/user_and_job_state";
 	
-	private static final String VER = "0.2.2";
+	private static final String VER = "0.2.3";
 
 	//required deploy parameters:
 	public static final String HOST = "mongodb-host";
