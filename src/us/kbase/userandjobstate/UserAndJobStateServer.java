0fc429ac883226f7416364fc7200b0297fe35003
Gavin Price <gaprice@lbl.gov>
2019-02-20 10:24:10 -0800

    Bump version

--- src/us/kbase/userandjobstate/UserAndJobStateServer.java
+++ src/us/kbase/userandjobstate/UserAndJobStateServer.java
@@ -146,7 +146,7 @@ public class UserAndJobStateServer extends JsonServerServlet {
 	private static final String GIT =
 			"https://github.com/kbase/user_and_job_state";
 	
-	private static final String VER = "0.2.3";
+	private static final String VER = "0.2.4";
 
 	//required deploy parameters:
 	public static final String HOST = "mongodb-host";
